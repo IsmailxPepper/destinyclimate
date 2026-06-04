bd6885515db150dad9bca89755518d72c822d8da
Ismail x Pepper <93700134+IsmailxPepper@users.noreply.github.com>
2026-06-04 11:56:44 +0530

    Update location.js

--- js/location.js
+++ js/location.js
@@ -1,1 +1,20 @@
+function getCurrentLocation(){
 
+    if(!navigator.geolocation){
+
+        return;
+
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        (position)=>{
+
+            console.log(
+                position.coords.latitude,
+                position.coords.longitude
+            );
+
+        }
+    );
+
+}
