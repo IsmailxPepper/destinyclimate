ae74f0eabf25def0a19a0cb1732cd97527e9cdb2
Ismail x Pepper <93700134+IsmailxPepper@users.noreply.github.com>
2026-06-04 14:49:35 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -93,3 +93,41 @@ function renderHourlyForecast(hourlyData){
     }
 
 }
+function getWeatherIcon(code){
+
+    if(code === 0) return "☀️";
+
+    if(
+        code === 1 ||
+        code === 2
+    ) return "🌤️";
+
+    if(code === 3)
+    return "☁️";
+
+    if(
+        code >= 45 &&
+        code <= 48
+    ) return "🌫️";
+
+    if(
+        code >= 51 &&
+        code <= 67
+    ) return "🌧️";
+
+    if(
+        code >= 71 &&
+        code <= 77
+    ) return "❄️";
+
+    if(
+        code >= 80 &&
+        code <= 82
+    ) return "🌦️";
+
+    if(
+        code >= 95
+    ) return "⛈️";
+
+    return "☁️";
+}
