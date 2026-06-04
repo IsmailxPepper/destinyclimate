4cef72dfa9062ba39e2d8d39e8d99015115a75d9
Ismail x Pepper <93700134+IsmailxPepper@users.noreply.github.com>
2026-06-04 11:58:06 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener(
+"DOMContentLoaded",
+() => {
 
-    console.log("DesTinY Climate™ Loaded");
+    const searchBtn =
+    document.getElementById(
+        "searchBtn"
+    );
+
+    const cityInput =
+    document.getElementById(
+        "citySearch"
+    );
+
+    searchBtn.addEventListener(
+        "click",
+        () => {
+
+            const city =
+            cityInput.value.trim();
+
+            if(city){
+
+                searchCityWeather(
+                    city
+                );
+
+            }
+
+        }
+    );
 
 });
